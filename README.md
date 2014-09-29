d508b0bea3851964eeb6af6cf1996f3ae61880c6
Roy <roy@bocistudio.com>
2014-09-29 13:04:24 +0700

    the actual javascript and stylesheet, update README.md

--- README.md
+++ README.md
@@ -9,19 +9,25 @@ A simple jQuery library to remove injected advertisement from u-ad.info
 
 ## Usage
 
+Add both `u-ad.info-remover.js` and `u-ad.info-remover.css` to your site after jquery.
+
+Then add the following code into your site main javascript file, modify as needed.
+
 ```javascript
-$.slowdyAdsRemover.init({
-  existedFunc: function(){
-    // advertisement detected, do something here if necessary,
-    // for example disabling turbolinks for rails app :
-    // $('body').attr('data-no-turbolink', '1')
-  },
-  cleanFunc: function(){
-    // advertisement not detected, do something here if necessary,
-    // for example congratulate visitor for not using scumbag ISP
-  },
-  popup: true, // default: true, set false to disable warning popup
-  popupText: 'You\'re using scumbag ISP please change it immediately.' // optional, change popupText
+jQuery(function(){
+  jQuery.slowdyAdsRemover.init({
+    existedFunc: function(){
+      // advertisement detected, do something here if necessary,
+      // for example disabling turbolinks for rails app :
+      // jQuery('body').attr('data-no-turbolink', '1')
+    },
+    cleanFunc: function(){
+      // advertisement not detected, do something here if necessary,
+      // for example congratulate visitor for not using scumbag ISP
+    },
+    popup: true, // default: true, set false to disable warning popup
+    popupText: 'You\'re using scumbag ISP please change it immediately.' // optional, change popupText
+  });
 });
 ```
 
---
+++ u-ad.info-remover.js
@@ -0,0 +1,85 @@
+/* ------------------------------------------------
+ *
+ * u-ad.info-remover
+ * (roy@bocistudio.com)
+ *
+ * ------------------------------------------------
+ * Requirements:
+ * ------------------------------------------------
+ *
+ * - jQuery
+ *
+ * ------------------------------------------------
+ * Usage:
+ * ------------------------------------------------
+ *
+ * See README.md
+ *
+ * ------------------------------------------------
+ */
+
+(function($) {
+  $.slowdyAdsRemover = {
+    _setCookie: function(c_name, value, exdays){
+      var c_value = escape(value);
+      if(exdays){
+        var exdate=new Date();
+        exdate.setDate(exdate.getDate() + exdays);
+        c_value = c_value + '; expires=' + exdate.toUTCString();
+      }
+      document.cookie=c_name + '=' + c_value + '; path=/';
+    },
+    _getCookie: function(c_name){
+      var c_value = document.cookie;
+      var c_start = c_value.indexOf(' ' + c_name + '=');
+      if (c_start == -1){
+        c_start = c_value.indexOf(c_name + '=');
+      }
+      if (c_start == -1){
+        c_value = null;
+      } else {
+        c_start = c_value.indexOf('=', c_start) + 1;
+        var c_end = c_value.indexOf(';', c_start);
+        if (c_end == -1){
+          c_end = c_value.length;
+        }
+        c_value = unescape(c_value.substring(c_start,c_end));
+      }
+      return c_value;
+    },
+    showPopup: function(container, options){
+      if(options.popup === false || $.slowdyAdsRemover._getCookie('slowdy-ads-remover-off')) return;
+      var $container = $(container);
+      var $popup = $container.find('.slowdy-ads-remover-popup');
+      if(!$popup.length){
+        $popup = $('<div class="slowdy-ads-remover-popup">').hide().appendTo($container);
+        $popup.append('<p>' + ((options.popupText) ? options.popupText :
+          'We have detected that you have advertisement injected by your ISP,\
+           this advertisement will break our site functionality so we disables it for you and alter our site functionality a bit.\
+           More information available <a href="https://github.com/Maysora/u-ad.info-remover/wiki/Information-(Informasi)" target="_blank">here</a>') +
+          '</p>');
+        $popup.append('<div><a href="#">Got it</a> | <a href="#" data-remember="true">Don\'t remind me again</a></div>')
+        $popup.on('click', 'a', function(){
+          $popup.slideUp();
+          $.slowdyAdsRemover._setCookie('slowdy-ads-remover-off', '1', ($(this).data('remember')) ? 99999 : false);
+        });
+      }
+      $popup.slideDown();
+    },
+    init: function(options){
+      if(!options) options = {};
+      var $container = $('body');
+      if(!$container.hasClass('slowdy-ads-remover')){
+        $container.addClass('slowdy-ads-remover');
+        var $scumbagTelmokom = $('script,iframe', $container).filter('[src*="u-ad.info"]');
+        if($scumbagTelmokom.length){
+          if(options.existedFunc && $.isFunction(options.existedFunc)) options.existedFunc.call($container.get(0), $container);
+          $.slowdyAdsRemover.showPopup($container, options);
+          $scumbagTelmokom.remove();
+        } else {
+          if(options.cleanFunc && $.isFunction(options.cleanFunc)) options.cleanFunc.call($container.get(0), $container);
+        }
+      }
+    }
+  };
+})(jQuery);
